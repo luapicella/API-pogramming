155ed7d71e367383cb7249001a4048daf32841cd
luapicella <apc.luigi@gmail.com>
2022-03-26 20:17:20 +0100

    add second version of isbn

--- ex/isbn-verifier/src/lib.rs
+++ ex/isbn-verifier/src/lib.rs
@@ -1,5 +1,5 @@
 /// Determines whether the supplied string is a valid ISBN number
-pub fn is_valid_isbn(isbn: &str) -> bool {
+pub fn is_valid_isbn_my(isbn: &str) -> bool {
     isbn.chars().fold((0, 0, true), |(count, sum , valid), c|{
         match c {
             '0'..='9' => (count+1, (sum+(10-count)*c.to_digit(10).unwrap())%11, valid),
@@ -8,4 +8,22 @@ pub fn is_valid_isbn(isbn: &str) -> bool {
             _ => (0, 0, false)
         }
     }) == (10, 0, true)
+}
+
+
+
+pub fn is_valid_isbn(isbn: &str) -> bool {
+    let mut t = 0;
+    let mut w = 10;
+    for ch in isbn.chars(){
+        match ch {
+            '-' => continue,
+            'X' if w==1 => t += 10,
+            '0'..='9' => t += ch.to_digit(10).unwrap()*w,
+            _ => return false
+        }
+        if w==0 {return false}
+        w -= 1
+    }
+    t % 11 == 0 && w == 0
 }
